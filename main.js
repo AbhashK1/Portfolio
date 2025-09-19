37762b74ecc6f971cc99230f7492061715ffd19e
Abhash Kumar <abhash.kr99@gmail.com>
2025-09-20 00:18:16 +0530

    Initial Commit

---
+++ main.js
@@ -0,0 +1,5 @@
+import bay from "https://cdn.jsdelivr.net/npm/@dunks1980/bay.js@3.4.2/bay.min.mjs";
+//import bay from "http://127.0.0.1:3001/bay.mjs";
+import modal from 'https://cdn.jsdelivr.net/npm/@dunks1980/bay.js@3.4.2/components/modal.mjs';
+bay();
+bay.create('bay-modal', modal, ['open']);
